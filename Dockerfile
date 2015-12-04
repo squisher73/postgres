0621e97793c7adb3e15a99d228fee1dae913b722
Jens Albrecht <jens.albrecht@stud.hs-hannover.de>
2015-12-04 23:50:16 +0100

    postgres

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,4 @@
 FROM postgres:9.2
 COPY initpgdb.sh /
+USER postgres
+RUN /initpgdb.sh
--- initpgdb.sh
+++ initpgdb.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+function wait_for_database() {
+  until psql --help &> /dev/null`; do
+    sleep 1
+  done
+}
+
+echo "=> Starting DBMS"
+/etc/init.d/postgresql start > /dev/null &
+
+echo "=> Waiting for DBMS"
+wait_for_server
+
+echo "=> Executing the commands"
 psql -d template1 -U postgres --command "CREATE DATABASE wildflytest;"
 psql -d template1 -U postgres --command "CREATE USER adminjk8vliu WITH SUPERUSER PASSWORD 'Gw9kVkChXCqp';"
 psql -d template1 -U postgres --command "GRANT ALL PRIVILEGES ON DATABASE wildflytest TO adminjk8vliu;"
